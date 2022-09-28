0ef198c0efbe63fbdf62f6899da3a6eea0d3967d
Elyse Kriegel <55773029+elyskrie21@users.noreply.github.com>
2022-09-28 01:44:48 +0000

    Decided on making a trading bot instead of an arbitrage bot.

---
+++ tradebot-mv/tests/__init__.py
@@ -0,0 +1,17 @@
+x = "Nancy"
+print(x)
+
+# Combine numbers and text
+s = "My lucky number is %d, what is yours?" % 7
+print(s)
+
+# alternative method of combining numbers and text
+s = "My lucky number is " + str(7) + ", what is yours?"
+print(s)
+
+# print character by index
+print(x[0])
+
+# print piece of string
+print(x[0:3])
+
